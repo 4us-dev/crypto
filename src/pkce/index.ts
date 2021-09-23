a90185df288e49709fd338e1657f23a6a05a0dc4
Clairton Carneiro Luz <clairton.c.l@gmail.com>
2021-09-23 17:22:49 -0300

    exporting generateChallengeAsync

--- src/pkce/index.ts
+++ src/pkce/index.ts
@@ -1,2 +1,3 @@
 export { default as codeVerifierGenerator } from './codeVerifierGenerator';
 export { default as codeChallengeGenerator } from './codeChallengeGenerator';
+export { default as codeChallengeGeneratorAsync } from './codeChallengeGeneratorAsync';
